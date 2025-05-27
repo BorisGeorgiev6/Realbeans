f45a02f7ca44b476f4674c2e5a86a886fb7ea2b5
BorisGeorgiev6 <b_georgiev2005@abv.bg>
2025-05-27 19:07:35 +0200

    asdas

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "r475t6",
+  projectId: "yp6a3m",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
